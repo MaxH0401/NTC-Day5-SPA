b2bd413780f107f12dd052e72086eb4d218b79b6
MaxH0401 <maxhuang8441@gmail.com>
2022-02-21 15:40:23 +0800

    step06 : add Cart

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,32 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    serverPath: 'http://35.234.34.149'
+    serverPath: 'http://35.234.34.149',
+    cart: []
   },
   getters: {
   },
   mutations: {
+    // 判斷進購物車的是否為新商品
+    addCart(state, data) {
+      let isNewProduct = true
+      console.log('data：',data)
+      state.cart.map(function (product) {
+        console.log('product：',product)
+        if (product.id === data.product.id) {
+          product.number += data.number
+          isNewProduct = false
+        }//如果不是(產品Id在購物車裡出現)則更新購買數量
+        return product
+      })
+      console.log('isNewProduct?',isNewProduct)
+      if (isNewProduct) {//if isNewProduct = true
+        let newProduct = data.product
+        newProduct.number = data.number
+        state.cart.push(newProduct)
+      }//如果是(產品Id不曾出現)則新增產品項目到購物車
+      console.log('state.cart',state.cart)
+    }
   },
   actions: {
   },
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,7 +3,7 @@
     <div class="product-component">
       <ProductBox v-bind="product">
         <input type="number" v-model="number" /> &nbsp;&nbsp;
-        <button>加入購物車</button>
+        <button @click='addCart(product)'>加入購物車</button>
       </ProductBox>
     </div>
     <hr />
@@ -22,6 +22,7 @@ export default {
   data() {
     return {
       product: "",
+      number:0,
       serverPath: this.$store.state.serverPath,
     };
   },
@@ -30,6 +31,19 @@ export default {
       .get(`${this.serverPath}/products/${this.$route.params.productId}`)
       .then((response) => (this.product = response.data));
   },
+  methods:{
+      addCart: function(product){
+          if(product.quantity - this.number < 0){
+              alert('存貨不足')
+              return
+          }
+          this.$store.commit('addCart',{
+              product: product,
+              number: this.number
+          })
+          alert('add Cart')
+      }
+  }
 };
 </script>
 <style scoped>
